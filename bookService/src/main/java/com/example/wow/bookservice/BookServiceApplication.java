e1a7392841b968a49bc4efb047d1f15154268d21
PixarPresent <pixarpresent@gmail.com>
2025-03-19 16:05:00 +0300

    add: book-service

--- bookService/src/main/java/com/example/wow/bookservice/BookServiceApplication.java
+++ bookService/src/main/java/com/example/wow/bookservice/BookServiceApplication.java
@@ -1,17 +1,15 @@
-package com.example.wow.configclient;
+package com.example.wow.bookservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients(basePackages = "com.example.wow.configclient.util")
-public class ConfigClientApplication {
+public class BookServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigClientApplication.class, args);
+        SpringApplication.run(BookServiceApplication.class, args);
     }
 
 }
